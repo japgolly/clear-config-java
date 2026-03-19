ecaeae9147c9618e14dc18465a408e956644f919
David Barri <japgolly@gmail.com>
2026-03-19 15:13:25 +1100

    `ConfigSource.{mapKeyQueries,environment,systemProps}

--- src/main/java/japgolly/clearconfig/ConfigSource.java
+++ src/main/java/japgolly/clearconfig/ConfigSource.java
@@ -1,11 +1,29 @@
 package japgolly.clearconfig;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public interface ConfigSource {
 
     public String name();
+
     public String get(String key);
+
+    public default ConfigSource mapKeyQueries(Function<String, String> f) {
+        var self = this;
+        return new ConfigSource() {
+
+            @Override
+            public String name() {
+                return self.name();
+            }
+
+            @Override
+            public String get(String key) {
+                return self.get(f.apply(key));
+            }
+        };
+    }
 
     public static ConfigSource manual(String name, Map<String, String> map) {
         return new ConfigSource() {
@@ -21,4 +39,31 @@ public interface ConfigSource {
             }
         };
     }
+
+    public static ConfigSource environment = new ConfigSource() {
+
+        @Override
+        public String name() {
+            return "Environment";
+        }
+
+        @Override
+        public String get(String key) {
+            return System.getenv(key);
+        }
+    };
+
+    public static ConfigSource systemProps = new ConfigSource() {
+
+        @Override
+        public String name() {
+            return "System Properties";
+        }
+
+        @Override
+        public String get(String key) {
+            return System.getProperty(key);
+        }
+    };
+
 }
---
+++ src/test/java/japgolly/clearconfig/ConfigSourceTest.java
@@ -0,0 +1,16 @@
+package japgolly.clearconfig;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Map;
+import org.junit.Test;
+
+public class ConfigSourceTest {
+
+    @Test
+    public void testMapKeyQueries() {
+        var source = ConfigSource.manual("test", Map.of("BLAH.PORT", "1234"));
+        var mapped = source.mapKeyQueries(s -> s.toUpperCase());
+        assertEquals("1234", mapped.get("blah.port"));
+        assertEquals(null, mapped.get("other"));
+    }
+}
