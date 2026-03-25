fb07a4f18a97a70068b64b2e0b53d7174a75f731
David Barri <japgolly@gmail.com>
2026-03-25 14:39:10 +1100

    Refactor: rename cfgSrcs

--- src/main/java/japgolly/clearconfig/ConfigReport.java
+++ src/main/java/japgolly/clearconfig/ConfigReport.java
@@ -7,10 +7,10 @@ import japgolly.clearconfig.util.AsciiTable;
 import japgolly.clearconfig.util.Internals;
 
 public class ConfigReport {
-    private final ConfigSources sources;
+    private final ConfigSources cfgSrcs;
 
-    public ConfigReport(ConfigSources sources) {
-        this.sources = sources;
+    public ConfigReport(ConfigSources cfgSrcs) {
+        this.cfgSrcs = cfgSrcs;
     }
 
     @Override
@@ -23,26 +23,26 @@ public class ConfigReport {
     }
 
     public String sources() {
-        var count = sources.sources.size() + 1;
-        var sourceList = sources.sources.stream().map(s -> "  - " + s.name()).collect(Collectors.joining("\n"));
+        var count = cfgSrcs.sources.size() + 1;
+        var sourceList = cfgSrcs.sources.stream().map(s -> "  - " + s.name()).collect(Collectors.joining("\n"));
         return String.format("%d sources (highest to lowest priority):\n%s\n  - Default", count, sourceList);
     }
 
     public String used() {
-        var header = sources.sources.stream().map(s -> s.name()).collect(Collectors.toList());
+        var header = cfgSrcs.sources.stream().map(s -> s.name()).collect(Collectors.toList());
         header.addFirst("Key");
         header.addLast("Default");
-        var keys = sources.seen().keySet().stream().sorted().collect(Collectors.toList());
+        var keys = cfgSrcs.seen().keySet().stream().sorted().collect(Collectors.toList());
         var rows = keys.stream().map(this::seenRow).collect(Collectors.toList());
         var table = AsciiTable.withHeader(header, rows);
         return String.format("Used keys (%d):\n%s", rows.size(), table);
     }
 
     public String unused() {
-        var header = sources.sources.stream().map(s -> s.name()).collect(Collectors.toList());
+        var header = cfgSrcs.sources.stream().map(s -> s.name()).collect(Collectors.toList());
         header.addFirst("Key");
-        var seenKeys = sources.seen().keySet();
-        var allKeys = sources.sources.stream()
+        var seenKeys = cfgSrcs.seen().keySet();
+        var allKeys = cfgSrcs.sources.stream()
                 .flatMap(s -> s.all().keySet().stream())
                 .filter(k -> !seenKeys.contains(k))
                 .distinct()
@@ -54,8 +54,8 @@ public class ConfigReport {
     }
 
     private List<String> seenRow(String key) {
-        var ctx = sources.seen().get(key);
-        var row = sources.sources.stream().map(s -> cell(s.get(key), ctx.secret)).collect(Collectors.toList());
+        var ctx = cfgSrcs.seen().get(key);
+        var row = cfgSrcs.sources.stream().map(s -> cell(s.get(key), ctx.secret)).collect(Collectors.toList());
         row.addFirst(key);
         row.addLast(cell(ctx.defaultValue.map(v -> "" + v).orElse(null), ctx.secret));
         return row;
@@ -63,7 +63,7 @@ public class ConfigReport {
 
     private List<String> unusedRow(String key) {
         var secret = Internals.IMPLICITLY_SECRET.matcher(key).matches();
-        var row = sources.sources.stream().map(s -> cell(s.get(key), secret)).collect(Collectors.toList());
+        var row = cfgSrcs.sources.stream().map(s -> cell(s.get(key), secret)).collect(Collectors.toList());
         row.addFirst(key);
         return row;
     }
